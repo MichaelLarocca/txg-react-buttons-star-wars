6df8342d8d921ea7263f5cdb9a97ced35a70708a
Larocca <Larocca@RIVPRO1.net>
2023-11-01 16:54:02 -0400

    Created Star Wars buttons
    Created a Star Wars button component that uses React children props

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import ButtonStarWars from './components/ButtonStarWars';
+import { FaEmpire, FaRebel, FaMandalorian, FaJournalWhills, FaJediOrder, FaSith } from 'react-icons/fa';
+import { GiDeathStar } from 'react-icons/gi';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <main>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ButtonStarWars>
+          <FaRebel />
+            Rebel
+          <FaRebel />
+        </ButtonStarWars>
+        <ButtonStarWars>
+          <FaEmpire />
+            Empire
+          <FaEmpire />
+        </ButtonStarWars>      
+        <ButtonStarWars>
+          <FaMandalorian />
+            Mandalorian
+          <FaMandalorian />
+        </ButtonStarWars>
+      </div>  
+      <div>
+        <ButtonStarWars>
+          <FaJournalWhills />
+            Journal of the Whills
+          <FaJournalWhills />
+        </ButtonStarWars>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div>  
+        <ButtonStarWars>
+          <FaJediOrder />
+            Jedi Order
+          <FaJediOrder />
+        </ButtonStarWars>
+        <ButtonStarWars>
+          <FaSith />
+            Sith
+          <FaSith />
+        </ButtonStarWars>     
+        <ButtonStarWars>
+          <GiDeathStar />
+            Death Star
+          <GiDeathStar />
+        </ButtonStarWars>  
+      </div>          
+    </main>
   )
 }
 
---
+++ src/components/ButtonStarWars.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ButtonStarWars(props) {
+    return(
+        <button className="sw-button ">{props.children}</button>
+    )
+}
+
+export default ButtonStarWars;
