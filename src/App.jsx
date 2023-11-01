40f339790e6efce05a06a3e9b25059b27785701f
Larocca <Larocca@RIVPRO1.net>
2023-11-01 17:17:48 -0400

    Added title

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,54 @@ import { GiDeathStar } from 'react-icons/gi';
 
 function App() {
   return (
-    <main>
-      <div>
-        <ButtonStarWars>
-          <FaRebel />
-            Rebel
-          <FaRebel />
-        </ButtonStarWars>
-        <ButtonStarWars>
-          <FaEmpire />
-            Empire
-          <FaEmpire />
-        </ButtonStarWars>      
-        <ButtonStarWars>
-          <FaMandalorian />
-            Mandalorian
-          <FaMandalorian />
-        </ButtonStarWars>
-      </div>  
-      <div>
-        <ButtonStarWars>
-          <FaJournalWhills />
-            Journal of the Whills
-          <FaJournalWhills />
-        </ButtonStarWars>
-      </div>
-      <div>  
-        <ButtonStarWars>
-          <FaJediOrder />
-            Jedi Order
-          <FaJediOrder />
-        </ButtonStarWars>
-        <ButtonStarWars>
-          <FaSith />
-            Sith
-          <FaSith />
-        </ButtonStarWars>     
-        <ButtonStarWars>
-          <GiDeathStar />
-            Death Star
-          <GiDeathStar />
-        </ButtonStarWars>  
-      </div>          
-    </main>
+    <>
+      <header>
+        <h1>Creating Reusable React Buttons with Props.Children </h1>
+      </header>
+      <main>
+        <div>
+          <ButtonStarWars>
+            <FaRebel />
+              Rebel
+            <FaRebel />
+          </ButtonStarWars>
+          <ButtonStarWars>
+            <FaEmpire />
+              Empire
+            <FaEmpire />
+          </ButtonStarWars>      
+          <ButtonStarWars>
+            <FaMandalorian />
+              Mandalorian
+            <FaMandalorian />
+          </ButtonStarWars>
+        </div>  
+        <div>
+          <ButtonStarWars>
+            <FaJournalWhills />
+              Journal of the Whills
+            <FaJournalWhills />
+          </ButtonStarWars>
+        </div>
+        <div>  
+          <ButtonStarWars>
+            <FaJediOrder />
+              Jedi Order
+            <FaJediOrder />
+          </ButtonStarWars>
+          <ButtonStarWars>
+            <FaSith />
+              Sith
+            <FaSith />
+          </ButtonStarWars>     
+          <ButtonStarWars>
+            <GiDeathStar />
+              Death Star
+            <GiDeathStar />
+          </ButtonStarWars>  
+        </div>          
+      </main>
+    </>
   )
 }
 
